e2e2b6a6f18a8269075374fb027e880b92b81f9f
SaraKhS <sara.khiar.souri@alumnos.upm.es>
2024-09-23 15:49:40 +0200

    Se ha limpiado el código

--- prueba.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title('Youtube comment annalizer')
-url=st.text_input("Introduce el link del video: ")
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    url : [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40] }))
